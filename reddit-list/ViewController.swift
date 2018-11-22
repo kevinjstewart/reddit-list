0c07579c1fc9ad81376e5d76ef1d06bda4b03078
Kevin Stewart <kevinjstew@gmail.com>
2018-11-21 21:38:17 -0500

    Initial commit

---
+++ reddit-list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reddit-list
+//
+//  Created by Kevin Stewart on 2018-11-21.
+//  Copyright © 2018 Kevin Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
